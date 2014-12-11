f4caf4ff420fc6e1ab457f97fd51d3766d7c6556
Dante Soares <dms3@rice.edu>
2014-12-11 11:29:35 -0600

    Updated rspec; removed .ruby-version and .travis.yml

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -9,13 +9,15 @@ Rails.application.routes.draw do
     resources 'events', only: [:index]
     resources 'identifiers', only: [:create]
 
-    scope '/events' do
-      scope '/platforms' do
-      event_routes :multiple_choices, to: 'input_events#create_multiple_choice'
-      event_routes :free_responses, to: 'input_events#create_free_response'
-      event_routes :messages
-      event_routes :gradings
-      event_routes :tasks
+    scope 'events' do
+      scope 'platforms' do
+        event_routes :multiple_choices,
+                     to: 'input_events#create_multiple_choice'
+        event_routes :free_responses,
+                     to: 'input_events#create_free_response'
+        event_routes :messages
+        event_routes :gradings
+        event_routes :tasks
       end
     end
   end
