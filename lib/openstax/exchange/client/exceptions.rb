c9a2726c86dfed8397e440a10f0a223b7e8ed1e0
Kevin Burleigh <klb@kindling>
2015-01-10 22:32:14 -0500

    added better message and backtrace output

--- lib/openstax/exchange/client/exceptions.rb
+++ lib/openstax/exchange/client/exceptions.rb
@@ -5,10 +5,15 @@ module OpenStax
         def initialize(msg, original=$!)
           super(msg)
           @original = original
+          set_backtrace(original.backtrace)
         end
 
         def inspect
           [super, @original.inspect].join(" ")
+        end
+
+        def message
+          [super, "(#{@original.message})"].join(" ")
         end
       end
     end
